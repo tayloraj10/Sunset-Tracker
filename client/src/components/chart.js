2a48b4103f572d15f790bd416eaa0cd4b940507c
Taylor Johnson <tjohnson@metrocommercial.com>
2019-01-03 17:00:43 -0500

    continue work on chart and add labels

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import '../styles/chart.css';
 
 
 class Chart extends Component {
@@ -9,9 +10,14 @@ class Chart extends Component {
 
     drawChart() {
         const data = [12, 5, 6, 6, 9, 10];
-        const svg = d3.select(".chart").append("svg").attr("width", 700).attr("height", 300);
 
-        svg.selectAll("rect").data(data).enter().append("rect").attr("x", (d, i) => i * 30).attr("y", (d, i) => 300 - 10 * d).attr("width", 25).attr("height", (d, i) => d * 10).attr("fill", "green");
+        // create whole svg element
+        const svg = d3.select(".chart").append("svg").attr("width", 700).attr("height", 300).attr('class', 'chart-content');
+
+        // create each bar
+        svg.selectAll("rect").data(data).enter().append("rect").attr("x", (d, i) => i * 30).attr("y", (d, i) => 300 - 10 * d).attr("width", 26).attr("height", (d, i) => d * 10).attr("fill", "green");
+    
+        svg.selectAll('text').data(data).enter().append('text').text( (d) => d).attr('x', (d, i) => i * 30 ).attr('y', (d, i) => 300 - (10 * d) - 3)
     }
 
     render() {
@@ -19,7 +25,6 @@ class Chart extends Component {
       return (
         <div className='chart'>
             <p>Chart</p>
-            
         </div>
       );
     }
